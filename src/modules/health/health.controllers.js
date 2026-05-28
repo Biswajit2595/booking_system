220f9ddea711432e4bb778013a81f13b6db0065e
Biswajit Roy <broy16431@gmail.com>
2026-05-28 17:54:54 +0530

    adding ci/cd workflow

--- src/modules/health/health.controllers.js
+++ src/modules/health/health.controllers.js
@@ -23,3 +23,21 @@ export const readinessCheck = async (req, res) => {
     });
   }
 };
+
+
+export const cicdCheck = async (req, res) => {
+  try {
+
+    res.status(200).json({
+      status: "testing CI/CD Pipeline with GitHub Actions working"
+    });
+  } catch (error) {
+
+    console.error(error);
+  
+    res.status(500).json({
+      status: "not_working",
+      error: error.message
+    });
+  }
+};
--- src/modules/health/health.routes.js
+++ src/modules/health/health.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import {
   healthCheck,
+  cicdCheck,
   readinessCheck
 } from "./health.controllers.js";
 
 const router = express.Router();
 
 router.get("/", healthCheck);
+router.get("/cicd", cicdCheck);
 router.get("/ready", readinessCheck);
 
 export default router;
